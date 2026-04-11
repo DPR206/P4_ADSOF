a35018e89f5bff70d510debf7e076cad0359fb50
dunap <dunap@LAPTOP-2VAK2E2V>
2026-04-11 14:09:58 +0200

    El sensor que faltaba

---
+++ P4_ADSOF/src/sensores/SensorPresion.java
@@ -0,0 +1,64 @@
+/**
+ * Este paquete contiene las clases necesarias para gestionar sensores
+ */
+package sensores;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * Esta clase representa un sensor de presión
+ * @author Claudia Saiz Escribano y Duna Puente Romera. 
+ * @version 1.0
+ * Nombre del fichero: SensorPresion.java
+ * 
+ */
+public class SensorPresion extends Sensor{
+
+	private static int ids = 0;
+	private static String idType = "PRES_";
+	private static double cotaInferior = 300;
+	private static double cotaSuperior = 1100;
+	private static String unidad = "hPa";
+	
+	/**
+	 * @param id
+	 * @param offset
+	 * @param ultimaLectura
+	 * @param tiempoUltimaLectura
+	 * @param ultimaCalibracion
+	 * @param fechaInstalacion
+	 */
+	public SensorPresion(String id, double offset, double ultimaLectura, LocalDateTime tiempoUltimaLectura,
+			LocalDateTime ultimaCalibracion, LocalTime fechaInstalacion) {
+		super(id, offset, ultimaLectura, tiempoUltimaLectura, ultimaCalibracion, fechaInstalacion);
+	}
+	
+	/**
+	 * 
+	 * @param offset
+	 * @param ultimaLectura
+	 * @param tiempoUltimaLectura
+	 * @param ultimaCalibracion
+	 * @param fechaInstalacion
+	 */
+	public SensorPresion(double offset, double ultimaLectura, LocalDateTime tiempoUltimaLectura,
+			LocalDateTime ultimaCalibracion, LocalTime fechaInstalacion) {
+		super(idType+String.format("%04d", ids), offset, ultimaLectura, tiempoUltimaLectura, ultimaCalibracion, fechaInstalacion);
+		ids++;
+	}
+
+	@Override
+	public String ultimaLectura() {
+		return this.getUltimaLectura()+SensorPresion.unidad;
+	}
+
+	@Override
+	public boolean valorValido(double valor) {
+		if(valor < SensorPresion.cotaInferior || valor > SensorPresion.cotaSuperior)
+			return false;
+		return true;
+	}
+
+	
+}
