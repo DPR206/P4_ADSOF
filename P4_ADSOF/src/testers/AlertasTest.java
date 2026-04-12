10e43708002f217e0115056fc3e246f4f7fc576a
dunap <dunap@LAPTOP-2VAK2E2V>
2026-04-12 19:50:07 +0200

    Test alertas sin acabar

---
+++ P4_ADSOF/src/testers/AlertasTest.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package testers;
+
+/**
+ * Esta clase representa un test de las alertas
+ * 
+ * @author Claudia Saiz Escribano y Duna Puente Romera.
+ * @version 1.0 Nombre del fichero: EstacionMeteorologica.java
+ * 
+ */
+public class AlertasTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
